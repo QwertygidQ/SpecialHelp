8d9bea014efac9ec870ed5237092e77c0589f0c3
QwertygidQ <Qwertygid@gmail.com>
2018-02-15 21:44:08 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from app import app, db
 from app.models import User, Service, Comment, Business
 
+
 @app.shell_context_processor
 def shell_cont():
     return {'db': db, 'User': User, 'Service': Service, 'Comment': Comment, 'Business': Business}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
