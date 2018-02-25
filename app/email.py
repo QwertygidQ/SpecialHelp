36853308cf2ae7f1dbad8dd1fb0ec7131d9a67bc
QwertygidQ <Qwertygid@gmail.com>
2018-02-26 00:24:48 +0300

    Added missing files

---
+++ app/email.py
@@ -0,0 +1,19 @@
+from flask_mail import Message
+from app import mail, app
+from flask import render_template
+
+
+def send_message(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients, body=text_body, html=html_body)
+    mail.send(msg)
+
+
+def send_reset_password_email(user):
+    token = user.create_reset_password_token()
+    send_message('[PROECTOS] Сброс пароля', app.config['MAIL_USERNAME'], [user.email],
+                 text_body=render_template('reset_password_email.txt',
+                                           user=user,
+                                           token=token),
+                 html_body=render_template('reset_password_email.html',
+                                           user=user,
+                                           token=token))
