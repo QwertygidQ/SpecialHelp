9628197c0657eb75baac244083d461ff60496e92
QwertygidQ <Qwertygid@gmail.com>
2018-03-25 00:14:56 +0300

    Rewrote the nl2br filter, so it actually works as intended

--- app/nl2br.py
+++ app/nl2br.py
@@ -4,14 +4,15 @@ import re
 from . import app
 from jinja2 import evalcontextfilter, Markup, escape
 
-_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
+_paragraph_re = re.compile('\r\n|\r|\n')
 
 
 @app.template_filter()
 @evalcontextfilter
 def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n')
-                          for p in _paragraph_re.split(escape(value)))
+    lines = _paragraph_re.split(escape(value))
+    result = '<br>\n'.join(lines)
+
     if eval_ctx.autoescape:
         result = Markup(result)
     return result
