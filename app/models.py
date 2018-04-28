7537c90d972ccef4d959f7ec154727b0a93c7c17
syn <isaqtm@gmail.com>
2018-04-28 05:59:31 +0300

    One of postgres layers likes strings

--- app/models.py
+++ app/models.py
@@ -28,7 +28,7 @@ class User(db.Model, UserMixin):
         return '<User {}; {}>'.format(self.username, self.email)
 
     def set_password(self, password):
-        self.password_hash = bcrypt.generate_password_hash(password)
+        self.password_hash = bcrypt.generate_password_hash(password).decode('ascii')
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
