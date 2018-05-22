e1b9d13f3b8687caae3133b07f9ddb7cb60846c0
QwertygidQ <Qwertygid@gmail.com>
2018-05-22 14:43:47 +0300

    done for wsgi

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from app import app, db
+from app import app as application
+from app import db
 from app.models import User, Tag, Comment, Business
 import config
 import sys
 
 
-@app.shell_context_processor
+@application.shell_context_processor
 def shell_cont():
     return {'db': db, 'User': User, 'Tag': Tag, 'Comment': Comment, 'Business': Business}
 
@@ -12,4 +13,4 @@ def shell_cont():
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1].isdigit() and 1 < int(sys.argv[1]) < 65535:
         config.port = int(sys.argv[1])
-    app.run(host=config.host, port=config.port, debug=config.debug)
+    application.run(host=config.host, port=config.port, debug=config.debug)
