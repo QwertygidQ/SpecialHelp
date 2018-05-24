ab6d657c9ba5bf8d2ed4c20fea9c558e4833a422
QwertygidQ <Qwertygid@gmail.com>
2018-05-24 11:47:07 +0300

    Saving work & using lots of new CDNs

--- app/static/js/semantic_rating_init.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.input-disallowed.rating').rating('disable');
-
-    $('.input-allowed.ui.rating').rating(
-        'setting', 'onRate', function(value){
-            $('#rating').val(value);
-        }
-    );
-});
