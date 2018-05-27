690b193b528641d38757a58642d9c8943eeda714
QwertygidQ <Qwertygid@gmail.com>
2018-05-27 12:41:07 +0300

    nl2br returns!

---
+++ app/nl2br.py
@@ -0,0 +1,17 @@
+import re
+from . import app
+from jinja2 import evalcontextfilter, Markup, escape
+
+_paragraph_re = re.compile('\r\n|\r|\n')
+
+
+@app.template_filter
+@evalcontextfilter
+def nl2br(eval_ctx, value):
+    lines = _paragraph_re.split(escape(value))
+    result = '<br/>'.join(lines)
+
+    if eval_ctx.autoescape:
+        result = Markup(result)
+
+    return result
