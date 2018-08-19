a396d41b8cf656c6d3f3ee31dfb49307bf6e1cce
Ivan Lysenko <Qwertygid@gmail.com>
2018-08-19 22:06:08 +0300

    Beautified JS code

--- app/static/js/map_init.js
+++ app/static/js/map_init.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     let map = L.map('business_map').setView(coords, zoom);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'       
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    
+
     L.marker(coords).addTo(map);
 });
