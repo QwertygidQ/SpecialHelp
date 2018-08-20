20adc2c994f1f3ecf3ee627f72c2d900bd9df15e
QwertygidQ <Qwertygid@gmail.com>
2018-08-20 16:15:18 +0300

    Small change

--- app/views.py
+++ app/views.py
@@ -224,7 +224,7 @@ def business_page(business_link):
 
 @app.route('/t/<tag_name>')
 def tag_list_page(tag_name):
-    if tag_name is None or Tag.query.filter_by(name=tag_name).count() == 0:
+    if tag_name is None or Tag.query.filter_by(name=tag_name).first() is None:
         abort(404)
 
     return render_template('tag_list.html',
